4f57bf3ac9409311cf1a5bb4205aceb3f593e650
vedashree <vedashree39555@gmail.com>
2022-07-01 00:41:24 +0530

    .class and .java files for tree, cluster, activation vector

---
+++ ClusterVis/src/ActivationVector.java
@@ -0,0 +1,3 @@
+public class ActivationVector {
+	 int[] actvector;
+}
---
+++ ClusterVis/src/tree.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public class tree {
+	ArrayList<cluster> allclusters;
+	int[] parentchild,  relations; 
+
+}
